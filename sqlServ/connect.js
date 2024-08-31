99fff1e4ffd7caa390918693ff7bbda136c90b38
navaax <157685350+navaax@users.noreply.github.com>
2024-08-31 20:40:35 +0000

    Pending changes exported from your codespace

--- sqlServ/connect.js
+++ sqlServ/connect.js
@@ -1,17 +1,31 @@
-import  mysql from 'mysql';
+import mysql from 'mysql';
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'remote',
-  password: '1234',
-  database: 'tablas'
-});
 
-connection.connect((err) => {
-  if (err) {
-    console.error('Error al conectar:', err);
-    return;
+
+const connectionConfig = {
+  host: "localhost",
+  user: "root",
+  password: "", // Replace with your password (if applicable)
+  // database: "rmb_server"
+};
+
+async function connectToMySQL() {
+  try {
+    const connection = mysql.createConnection(connectionConfig);
+    const trueConnect = await connection.connect();
+    if(trueConnect == true){
+      console.log('Conectado');
+    }else{
+      console.log('Problemas en la conexion');
+    }
+
+    // Perform database operations here using `connection`
+
+    await connection.end(); // Close the connection when done
+  } catch (err) {
+    console.error("Error connecting to MySQL:", err);
+    // Handle connection errors here (e.g., retry logic, logging)
   }
-  console.log('Conexión exitosa');
-});
+}
+
+connectToMySQL(); // Call the function to initiate the connection
